259d5f23a303b53d2bf6cdfa5da5eba4bc780118
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-02-01 22:09:33 +0300

    using local models

---
+++ UsingLocalModels/CustomHttpMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingKernels
+{
+    public class CustomHttpMessageHandler : HttpClientHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            // Custom logic before the request
+            Console.WriteLine($"Sending request to: {request.RequestUri}");
+
+            request.RequestUri = new Uri("http://localhost:1234/v1/chat/completions");
+
+            // Call base handler
+            var response = await base.SendAsync(request, cancellationToken);
+
+            // Custom logic after the request
+            Console.WriteLine($"Response status: {response.StatusCode}");
+
+            return response;
+        }
+    }
+}
---
+++ UsingLocalModels/Program.cs
@@ -0,0 +1,21 @@
+﻿// Create custom HTTP handler
+using BuildingKernels;
+using Microsoft.SemanticKernel;
+
+var handler = new CustomHttpMessageHandler();
+var httpClient = new HttpClient(handler);
+
+string modelId = "this is going to be ignored";
+
+// Create kernel builder with custom client
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion(modelId, "ApiKey", "OrgId", "ServiceId", httpClient)
+
+    .Build();
+
+
+var prompt = "Write a hello world program in C#";
+var result = await kernel.InvokePromptAsync(prompt);
+
+Console.WriteLine(result);
+Console.ReadKey();
